79379fb0e0339143a5bb8448804c48830d6667dc
zchaudhry11 <zchaudhry11@gmail.com>
2016-01-02 00:12:48 -0500

    moved portfolio item generation to javascript to clean up code

---
+++ js/portfolioGenerator.js
@@ -0,0 +1,79 @@
+//Add portfolio items and modals on document load
+buildPortfolio();
+
+function addItems()
+{
+    /* Row 1 */
+    //Item 1
+    $('#workRow1').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal1" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_1.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 2
+    $('#workRow1').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal2" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_2.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 3
+    $('#workRow1').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal3" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_3.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 4
+    $('#workRow1').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal4" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_4.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 5
+    $('#workRow1').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal5" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_5.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 6
+    $('#workRow1').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal4" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_6.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    /* Row 2 */
+    
+    //Item 7
+    $('#workRow2').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal7" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_7.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 8
+    $('#workRow2').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal8" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_8.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 9
+    $('#workRow2').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal9" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_9.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 10
+    $('#workRow2').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal10" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_10.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 11
+    $('#workRow2').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal11" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_11.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 12
+    $('#workRow2').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal12" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_12.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    /* Row 3 */
+    
+    //Item 13
+    $('#workRow3').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal13" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_13.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 14
+    $('#workRow3').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal14" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_14.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 15
+    $('#workRow3').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal15" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_15.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 16
+    $('#workRow3').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal16" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_16.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 17
+    $('#workRow3').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal17" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_17.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+    //Item 18
+    $('#workRow3').append($('<div class="col-sm-4 portfolio-item"><a href="#portfolioModal18" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_18.png" class="img-responsive" alt="..." draggable="false"></a></div>'));
+    
+}
+
+function addModals()
+{
+    
+}
+
+function buildPortfolio()
+{
+    addItems();
+}
+
+/*
+<div class="col-sm-4 portfolio-item"><a href="#portfolioModal1" class="portfolio-link" data-toggle="modal"><div class="caption"><div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div><img src="img/portfolio/portfolio_1.png" class="img-responsive" alt="..." draggable="false"></a></div>
+*/
